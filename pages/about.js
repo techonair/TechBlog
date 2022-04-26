f48b66c479a30d6c0148261475d653c688ca8338
Bhanu Pratap <techieonmail@gmail.com>
2022-04-26 21:01:52 +0530

    .

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>Let me tell you something about me</div>
+  )
+}
+
+export default About
---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>You can contact me through</div>
+  )
+}
+
+export default Contact
